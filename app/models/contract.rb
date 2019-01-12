e1a986dd5d4ffcf9def948340498220a3f721b17
Chip Kragt <chiperific@gmail.com>
2019-01-12 16:45:01 -0500

    seed the reports

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -3,6 +3,7 @@
 class Contract < ApplicationRecord
   has_many :targets, inverse_of: :contract, dependent: :destroy
   has_many :plans,   inverse_of: :contract, dependent: :destroy
+  has_many :reports, inverse_of: :contract
 
   validates_presence_of :start_date, :end_date
 
