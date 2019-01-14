0e0f898283a56f9863f56a007246bcac0a00b150
Chip Kragt <chiperific@gmail.com>
2019-01-14 11:53:36 -0500

    better email name for Devise, fix puma

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: '"20 Liters Tracker" track@20liters.org'
   layout 'mailer'
 end
