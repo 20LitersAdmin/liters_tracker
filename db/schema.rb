fa68f1ba874f93c1a6745d7653a1faf8e52ca74b
Chip Kragt <chiperific@gmail.com>
2019-08-24 15:35:43 -0400

    save old db file for safety

--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_07_194748) do
+ActiveRecord::Schema.define(version: 2019_08_19_020913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,7 +68,6 @@ ActiveRecord::Schema.define(version: 2019_07_07_194748) do
   create_table "plans", force: :cascade do |t|
     t.bigint "contract_id", null: false
     t.bigint "technology_id", null: false
-    t.string "model_gid", null: false
     t.integer "goal", null: false
     t.integer "people_goal"
     t.datetime "created_at", null: false
@@ -85,7 +84,6 @@ ActiveRecord::Schema.define(version: 2019_07_07_194748) do
     t.bigint "technology_id", null: false
     t.bigint "user_id", null: false
     t.bigint "contract_id", null: false
-    t.string "model_gid", null: false
     t.integer "distributed"
     t.integer "checked"
     t.datetime "created_at", null: false
