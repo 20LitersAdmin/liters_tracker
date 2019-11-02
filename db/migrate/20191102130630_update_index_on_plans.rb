a21c3fe12f000c040c1fb5e017de82412fecee5e
brandonklotz <brandonklotz@icloud.com>
2019-11-02 12:01:14 -0400

    Resolve Migration Naming error

--- db/migrate/20191102130630_update_index_on_plans.rb
+++ db/migrate/20191102130630_update_index_on_plans.rb
@@ -1,4 +1,4 @@
-class AddIndexToPlans < ActiveRecord::Migration[6.0]
+class UpdateIndexOnPlans < ActiveRecord::Migration[6.0]
   def change
     add_index :plans, :created_at
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_02_130630) do
+ActiveRecord::Schema.define(version: 2019_11_02_142156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -160,6 +160,7 @@ ActiveRecord::Schema.define(version: 2019_11_02_130630) do
     t.string "local_cost_currency", default: "USD", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["report_worthy"], name: "index_technologies_on_report_worthy"
   end
 
   create_table "users", force: :cascade do |t|
