dd4def576d2876e6db1e0cbf189caaa1890e5914
Chip Kragt <chiperific@gmail.com>
2019-11-01 23:19:53 -0400

    tweak the mailers

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = '"20 Liters Tracker" track@20liters.org'
+  config.mailer_sender = 'track@20liters.org'
 
   # Configure the class responsible to send e-mails.
   config.mailer = 'DeviseMailer'
