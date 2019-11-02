a2f9ceb7c680f3a1788773bbf97f5841a1732f6c
Zack <zackary.kendra@gmail.com>
2019-11-02 08:42:35 -0400

    move save image into its own function

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -2,31 +2,33 @@ class StoriesController < ApplicationController
 
 	def index
 	end
+	
 	def show
 	end
+
 	def new
 	end
 
 	def create
-	  puts params.inspect
+      save_image(params)
+    end
+	
+	private
+
+	def save_image(params)
 	  image_io = params[:stories][:picture]
 
 	  File.open(Rails.root.join('tmp', image_io.original_filename), 'wb') do |file|
         file.write(image_io.read)
       end
-      puts Rails.application.credentials.aws[:secret_access_key]
-      puts Rails.application.credentials.aws[:access_key_id]
-      s3 = Aws::S3::Resource.new(region:'us-east-2') #todo what region
+      
+      s3 = Aws::S3::Resource.new(
+      	region:'us-east-2',
+        credentials: Aws::Credentials.new(Rails.application.credentials.aws[:access_key], Rails.application.credentials.aws[:secret_key])
+      ) # todo what region
       obj = s3.bucket('20litres-images').object(image_io.original_filename)
-      puts obj.inspect
       obj.upload_file(Rails.root.join('tmp', image_io.original_filename))
-      puts obj.inspect
 
-	  # uploaded_io = params[:person][:picture]
-      # File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
-      #   file.write(uploaded_io.read)
-      # end
-    end
-	
+	end
 
 end
