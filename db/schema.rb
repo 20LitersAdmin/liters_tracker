864690c1dbfa3e207f79334001ad4f63e59b5894
CJ Davis <code@cjdavis.me>
2019-11-02 13:47:54 -0400

    Remove unused data from reports

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_02_171346) do
+ActiveRecord::Schema.define(version: 2019_11_02_175331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -109,7 +109,6 @@ ActiveRecord::Schema.define(version: 2019_11_02_171346) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "people"
-    t.integer "households"
     t.integer "reportable_id"
     t.string "reportable_type"
     t.index ["contract_id", "technology_id", "reportable_id", "reportable_type"], name: "idx_belongs_to_plan"
