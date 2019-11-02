0c566aaa37263ea0ac4d4526b932b1b8ff890028
Michael Doran <doranm09@gmail.com>
2019-11-02 15:23:40 -0400

    adding hero image

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -10,7 +10,7 @@ class DashboardController < ApplicationController
       { stat: technology.lifetime_impact, title: "#{technology.name}s" }
     end
 
-    @global_impact = Report.all.sum(:people)
+    # @global_impact = Report.all.sum(:people)
     @stories = Story.all
   end
 end
