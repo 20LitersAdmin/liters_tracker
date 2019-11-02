58daacf88fee346665ce19e1a3d56e7bc3ceab3b
brandonklotz <brandonklotz@icloud.com>
2019-11-02 08:02:18 -0400

    Add basic Carousel to the dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class DashboardController < ApplicationController
+  layout 'dashboard'
+
   def index
     @lifetime_stats = Technology.report_worthy.map do |technology|
       next if technology.lifetime_impact.zero?
