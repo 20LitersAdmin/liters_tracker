d196c34bd0c1ce7d2efcc08948baf7f221f7a9d8
Michael Doran <doranm09@gmail.com>
2019-11-03 11:40:04 -0500

    adding filter

--- app/models/story.rb
+++ app/models/story.rb
@@ -87,11 +87,23 @@ class Story < ApplicationRecord
     joins(:report).order('reports.date ASC').pluck('reports.date').uniq
   end
 
-  def self.bin_stories_by_year_and_month(collection_of_stories)
+  # helper function that can be used in the future to bin and sort all stories in the data base
+  #
+  #
+  # OUTPUT:
+  #
+  #
+  # all_stories = 
+  #  {
+  #
+  #     "2019": {"Jac" .. "Dec"} .. "XXX": {"Jac" .. "Dec"}
+  #  }
+  #
+  #
+  def self.bin_all_stories
     binned_stories = {} # variable for the final return of stories that are binned by month
     months = Date.const_get(:ABBR_MONTHNAMES).compact # returns nil as the first entry, so use compact to remove it
     years = self.get_story_years
-
     years.each do |y|
       tmp_hash = {}
       (1..12).step(1) do |m|
@@ -103,6 +115,29 @@ class Story < ApplicationRecord
       binned_stories[y.to_s] = tmp_hash # store off the hashes of stories associated by month and year
     end
     binned_stories
+  end
+
+
+  def self.bin_stories_by_year(year)
+    story_collection = nil
+    stories_by_month = {}
+    month_list = []
+    (1..12).step(1) do |m|
+      tmp_hash = {}
+      tmp_date = Date.new(year, m)
+      # start with the first month and build a hash of all stories that are binned by a month
+      tmp_stories = self.between_dates(tmp_date.beginning_of_month, tmp_date.end_of_month)
+      if tmp_stories.size > 0 # check if there are stories for this month
+        month_list << Date.const_get(:ABBR_MONTHNAMES)[m]
+        if story_collection.nil?
+          story_collection = tmp_stories # combine all the active record collections for the year
+        else
+          story_collection += tmp_stories
+        end
+        stories_by_month[Date.const_get(:ABBR_MONTHNAMES)[m]] = tmp_stories #convert digit to abbreviated month name
+      end
+    end
+    return story_collection, month_list, stories_by_month # return the whole collection of stories, the months where stories exists, and later if needed a hash where key=month, value=ActiveRecord collection
   end
 
   def self.get_story_years
