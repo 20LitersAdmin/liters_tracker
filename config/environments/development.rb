787e67e79f37947a441d9dcd86f5c6ebbea62a54
Zack <zackary.kendra@gmail.com>
2019-11-03 11:28:19 -0500

    remove 'use master key' from dev and prod

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,7 +63,6 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  config.require_master_key = true
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
