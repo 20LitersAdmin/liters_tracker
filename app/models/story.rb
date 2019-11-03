da826d66bab5ec5f4c3b4c38aeb66bd078058d39
CJ Davis <code@cjdavis.me>
2019-11-02 20:56:47 -0400

    Remove broken story scope

--- app/models/story.rb
+++ app/models/story.rb
@@ -3,7 +3,6 @@
 class Story < ApplicationRecord
   belongs_to :report
   scope :get_stories_by_year, ->(year_string) { joins(:report).where('reports.date BETWEEN ? AND ?', "#{year_string}-01-01", "#{year_string}-12-31")}
-  scope :stories_by_month
 
   def save_image(image_io)
 
