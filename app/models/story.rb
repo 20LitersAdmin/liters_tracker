251e09335b853ae558305ab2b67322cb0dcf64ec
Chip Kragt <chiperific@gmail.com>
2019-11-02 21:06:20 -0400

    reformat dashboard handler, implement dashboard index year loop

--- app/models/story.rb
+++ app/models/story.rb
@@ -3,7 +3,10 @@
 class Story < ApplicationRecord
   belongs_to :report
 
-  scope :get_stories_by_year, ->(year_string) { joins(:report).where('reports.date BETWEEN ? AND ?', "#{year_string}-01-01", "#{year_string}-12-31")}
-  scope: :stories_by_month
+  scope :between_dates, ->(start_date, end_date) { joins(:report).where('reports.date BETWEEN ? AND ?', start_date, end_date)}
+
+  def self.array_of_unique_dates
+    joins(:report).order('reports.date ASC').pluck('reports.date').uniq
+  end
 
 end
