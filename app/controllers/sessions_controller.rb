a5048dff84487d92666b4422043d6fd638c6589d
Chip Kragt <chiperific@gmail.com>
2022-01-28 14:17:07 -0500

    add handy password reset before login in development

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,21 +6,5 @@ class SessionsController < Devise::SessionsController
     User.first.reset_password('password', 'password') if Rails.env.development?
 
     super
-    # self.resource = resource_class.new(sign_in_params)
-    # clean_up_passwords(resource)
-    # yield resource if block_given?
-    # respond_with(resource, serialize_options(resource))
-  end
-
-  # def create
-  #   self.resource = warden.authenticate!(auth_options)
-  #   set_flash_message!(:notice, :signed_in)
-  #   sign_in(resource_name, resource)
-  #   yield resource if block_given?
-  #   respond_with resource, location: after_sign_in_path_for(resource)
-  # end
-
-  def update
-    super
   end
 end
